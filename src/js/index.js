53fcd25d771cf94d4e3a8e9ba647d5b723de9498
fadliselaz <fadliselaz@gmail.com>
2019-03-13 04:03:36 +0700

    add

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import store from "../js/store/index"
+import { addArticle, addUser } from "../js/actions/index"
+
+window.store = store
+window.addArticle = addArticle
+window.addUser = addUser
+
---
+++ src/js/reducers/index.js
@@ -0,0 +1,30 @@
+
+
+import { ADD_ARTICLE, ADD_USER } from "../constants/action-types"
+
+//imuntable STATE here
+const initialState = {
+    articles : [],
+    user : []
+}
+
+//reducer yang akan memproduksi state untuk aplikasimu..
+function rootReducer(state=initialState, action){
+    if(action.type === ADD_ARTICLE){
+        return Object.assign({}, state, {
+            articles: state.articles.concat(action.payload)
+        })
+    }else if(action.type === ADD_USER){
+        return Object.assign({}, state, {
+            user: state.user.concat(action.payload)
+        })
+    }
+    return state
+}
+
+//bisa dilihat diatas, karena kita tahu bahwa state pada redux
+// merupakan IMUTABLE maka harus menggunakan Object.assign agar tetep imun
+
+
+export default rootReducer
+
