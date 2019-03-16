e8df7829ad54623c412851bcfacc207e4cc71a88
fadliselaz <fadliselaz@gmail.com>
2019-03-16 23:08:14 +0700

    16/3

---
+++ src/js/component/list.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux"
+
+const mapStateToProps = state => {
+    return {articles: state.articles}
+}
+
+const connectedList = ({articles}) => (
+    <ul>
+        {articles.map(el => (
+            <li key={el.id}>
+            {el.title}
+            </li>
+        ))}
+    </ul>
+)
+
+const List = connect(mapStateToProps)(connectedList)
+export default List
